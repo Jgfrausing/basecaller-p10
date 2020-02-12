287c5aa5734afdc86db9b76adbb4c5838362e853
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2020-02-12 17:17:13 +0100

    Implement relatively inefficient metric calc func for sequence comparison

--- bc/bc/tests/test_sequence.py
+++ bc/bc/tests/test_sequence.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 from typing import List, Dict
 
-import bc.utils.assembly as assembly
+import bc.utils.sequence as sequence
 
 
-class TestAssembly(TestCase):
+class TestSequence(TestCase):
     def test_assembles_correctly(self):
         # Arrange
         reads: List[str] = ['AAGGCCTAGCT',
@@ -14,7 +14,19 @@ class TestAssembly(TestCase):
         alphabet: Dict[int, str] = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
 
         # Act
-        result: str = assembly.assemble(reads, window_size, stride, alphabet)
+        result: str = sequence.assemble(reads, window_size, stride, alphabet)
 
         # Assert
         self.assertEqual("AAGGCCTAGCTA", result)
+
+    def test_calcs_correct_identity(self):
+        # Arrange
+        s1: str = 'AAGGCC'
+        s2: str = 'AAAGGCCA'
+
+        # Act
+        metrics = sequence.calc_sequence_error_metrics(s1, s2)
+        _, rate_identity, _, _, _ = metrics
+
+        # Assert
+        self.assertEqual(rate_identity, 1)
