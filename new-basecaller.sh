2e8886651b73d9a564969508feea9bea4010ed76
jgfrausing <jfraus14@student.aau.dk>
2020-03-24 14:48:34 +0100

    Add catch for missing parameter

--- new-basecaller.sh
+++ new-basecaller.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-mkdir model-nbk/$1
-cp model-nbk/template/Model-template.ipynb model-nbk/$1/$1.ipynb
-echo $1 created
+if [ "$#" -eq  "1" ]
+	then
+		mkdir model-nbk/$1
+		cp model-nbk/template/Model-template.ipynb model-nbk/$1/$1.ipynb
+		echo $1 created
+	else
+		echo Missing parameter: name
+
+fi
