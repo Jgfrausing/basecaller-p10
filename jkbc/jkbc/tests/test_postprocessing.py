2962190cd41d073ec11ee6c03b8ff6b67b4277fe
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2020-03-25 11:33:47 +0100

    Create and fix tests for pre and postprocessing

--- jkbc/jkbc/tests/test_postprocessing.py
+++ jkbc/jkbc/tests/test_postprocessing.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 
 import jkbc.utils.postprocessing as pp
+import pytest
 
 
 def test_assembles_correctly():
@@ -17,16 +18,46 @@ def test_assembles_correctly():
     # Assert
     assert "AAGGCCTAGCTA" == result
 
-def test_calcs_correct_identity():
+def test_calc_sequence_error_metrics_identical():
+    # Arrange
+    s: str = 'AAGGCCTTGGCC'
+
+    # Act
+    rates = pp.calc_sequence_error_metrics(s, s)
+
+    # Assert
+    assert rates.identity  == pytest.approx(1)
+    assert rates.error     == pytest.approx(0)
+    assert rates.mismatch  == pytest.approx(0)
+    assert rates.deletion  == pytest.approx(0)
+    assert rates.insertion == pytest.approx(0)
+
+def test_calc_sequence_error_metrics_third_mismatch():
     # Arrange
     s1: str = 'AAGGCC'
-    s2: str = 'AAAGGCCA'
+    s2: str = 'AATTCC'
 
     # Act
     rates = pp.calc_sequence_error_metrics(s1, s2)
 
     # Assert
-    assert rates.identity == 1
+    assert rates.mismatch == pytest.approx(1/3)
+
+def test_calc_sequence_error_metrics_different():
+    # Arrange
+    s1: str = 'AAGGCCTT'
+    s2: str = 'AATTCC'
+
+    # Act
+    rates = pp.calc_sequence_error_metrics(s1, s2)
+
+    # Assert
+    assert rates.mismatch == pytest.approx(1/4)
+    assert rates.insertion == pytest.approx(0)
+    assert rates.deletion == pytest.approx(1/4)
+    assert rates.identity == pytest.approx(1 - 1/4 - 1/4) # 1 - deletions - mismatch
+    assert rates.error == pytest.approx(1/4 + 0 + 1/4) # mismatch + insertion + deletion
+
 
 def test_convert_idx_to_base_sequence_simple():
     # Arrange
--- jkbc/jkbc/tests/test_preprocessing.py
+++ jkbc/jkbc/tests/test_preprocessing.py
@@ -5,17 +5,16 @@ def test_add_label_padding_simple():
   # Arrange
   labels = [[1, 2, 3], [2, 3]]
   fixed_label_len = 5
-  padding_id = 4
 
   # Act
-  result = pp.add_label_padding(labels, fixed_label_len, padding_id)
+  result = pp.add_label_padding(labels, fixed_label_len)
 
   # Assert
-  np.testing.assert_array_equal(result, [[1, 2, 3, 4, 4], [2, 3, 4, 4, 4]])
+  np.testing.assert_array_equal(result, [[1, 2, 3, 0, 0], [2, 3, 0, 0, 0]])
 
 
-def test_succesfull_read():
-    elm = prep.SignalCollection("../mapped_umi16to9.hdf5")[264]
+def test_succesful_read():
+    elm = pp.SignalCollection("../mapped_umi16to9.hdf5")[264]
     assert len(elm.x) > 0
     assert len(elm.y) > 0
     assert len(elm.reference) > 0
