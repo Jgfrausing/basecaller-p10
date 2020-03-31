4fd1c15162981afa17ba8a863d9632d762b70481
jgfrausing <jfraus14@student.aau.dk>
2020-03-31 10:42:45 +0200

    Update template + chiron with callback for save model

--- new-basecaller.sh
+++ new-basecaller.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 if [ "$#" -eq  "1" ]
 	then
-		mkdir model-nbk/$1
-		cp model-nbk/template/Model-template.ipynb model-nbk/$1/$1.ipynb
+		nbPath="model-nbk/$1/$1.ipynb"
+		if test -f "$nbPath"; 
+		then
+	 		echo "A basecaller with that name already exist"
+			exit 1
+		fi
+		mkdir -p model-nbk/$1
+		cp model-nbk/template/Model-template.ipynb $nbPath 
 		echo $1 created
 	else
 		echo Missing parameter: name
-
 fi
