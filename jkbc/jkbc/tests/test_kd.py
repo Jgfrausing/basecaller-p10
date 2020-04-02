5ec1d640d3af092959c0b773ef863475593f7ab6
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2020-04-02 12:08:41 +0200

    Add test that checks correct ordering of data for kd feather generation

--- jkbc/jkbc/tests/test_kd.py
+++ jkbc/jkbc/tests/test_kd.py
@@ -2,6 +2,8 @@ import shutil
 import torch
 import torch.nn as nn
 
+import numpy as np
+
 import jkbc.utils.kd as kd
 import jkbc.utils.files as f
 
@@ -20,7 +22,7 @@ def teardown_function(function):
     shutil.rmtree(FOLDER_PATH)
 
 
-def test_generate_and_save_kd_data_from_teacher_with_load():
+def test_generate_and_save_y_teacher_with_load():
   # Arrange
   teacher_model = TeacherModel()
   teacher_name = "teacher_kd"
@@ -28,12 +30,28 @@ def test_generate_and_save_kd_data_from_teacher_with_load():
   x = torch.rand((N, FEATURES, WS))
   
   # Act
-  y_teacher_kd = kd.generate_and_save_y_teacher(FOLDER_PATH, teacher_name, x, teacher_model, bs = 2, ws = WS)
+  kd.generate_and_save_y_teacher(FOLDER_PATH, teacher_name, x, teacher_model, bs = 2, ws = WS)
   y_teacher_f = f.read_kd_y_teacher_from_feather_file(FOLDER_PATH, teacher_name, OUT_SIZE, ALPHABET_SIZE)
   
   # Assert
   assert y_teacher_f.shape == (N, OUT_SIZE, ALPHABET_SIZE)
   ## Consider checking that y_teacher_f == y_teacher_kd
+  
+  
+def test_generate_and_save_y_teacher_correct_ordering():
+  # Arrange
+  teacher_model = TeacherModel()
+  teacher_name = "teacher_ordering"
+  x = torch.randint(10, (N, FEATURES, WS), dtype=torch.float)
+  y_teacher = teacher_model(x).detach()
+  
+  # Act 
+  kd.generate_and_save_y_teacher(FOLDER_PATH, teacher_name, x, teacher_model, bs = 2, ws = WS)
+  y_teacher_f = torch.tensor(f.read_kd_y_teacher_from_feather_file(FOLDER_PATH, teacher_name, OUT_SIZE, ALPHABET_SIZE)).detach()
+  
+  # Assert
+  DECIMAL_PRECISION = 5
+  np.testing.assert_almost_equal(y_teacher.numpy(), y_teacher_f.numpy(), decimal=DECIMAL_PRECISION)
   
   
 class TeacherModel(nn.Module):
@@ -46,3 +64,4 @@ class TeacherModel(nn.Module):
     xb = xb.view(bs, -1)
     xb = self.lin(xb)
     return xb.view((bs, OUT_SIZE, ALPHABET_SIZE))
+  
