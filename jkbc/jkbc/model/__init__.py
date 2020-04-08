86e1ec5c929fd9466ccbaa6b9ff2995925211e4f
jgfrausing <jfraus14@student.aau.dk>
2020-04-08 09:07:39 +0200

    Fix error in predict

--- jkbc/jkbc/model/__init__.py
+++ jkbc/jkbc/model/__init__.py
@@ -13,7 +13,7 @@ def predict(learner, x: t.Tensor, alphabet: t.Dict[int, str], window_size, strid
     alphabet_str = ''.join(alphabet_values)
 
     # Predict signals
-    pred = learner.pred_batch(x).detach()
+    pred = learner.model(x).detach().cpu()
     # Decode into characters using beam search
     decoded = pop.decode(pred, alphabet_str, beam_size=beam_size, threshold=beam_threshold)
     # Assemble most likely sequence
@@ -30,6 +30,26 @@ def predict(learner, x: t.Tensor, alphabet: t.Dict[int, str], window_size, strid
     return assembled, (accuracy, alignment)
 
 
+def predict_single(learner, x: t.Tensor, alphabet: t.Dict[int, str], reference: str=None, beam_size = 25, beam_threshold=0.1) -> t.Tuple[str, t.Tuple[float, str]]:
+    alphabet_values = list(alphabet.values())
+    alphabet_str = ''.join(alphabet_values)
+
+    # Predict signals
+    pred = learner.pred_batch(x).detach()
+    # Decode into characters using beam search
+    decoded = pop.decode(pred, alphabet_str, beam_size=beam_size, threshold=beam_threshold)
+    # Assemble most likely sequence
+    assembled = pop.assemble(decoded, window_size, stride, alphabet)
+    
+    # Return assembled, if no reference is available for comparison
+    if reference is None: 
+        return assembled, None
+    
+    # Convert reference (list[int]) to string
+    actual = pop.convert_idx_to_base_sequence(reference, alphabet_values)
+    # Get accuracy
+    accuracy, alignment = pop.calc_accuracy(actual, assembled, return_alignment=True)
+    return assembled, (accuracy, alignment)
 def load_model_weights(learner, model_name):
     try:
         if not model_name:
--- jkbc/jkbc/utils/bonito/decode.py
+++ jkbc/jkbc/utils/bonito/decode.py
@@ -4,7 +4,7 @@ def align(ref, seq):
     """
     Get (and print) the alignment between `ref` and `seq`
     """
-    alignment = parasail.nw_trace_striped_32(ref, seq, 8, 4, parasail.dnafull)
-    align_string = '\n'.join([alignment.traceback.query, alignment.traceback.comp, alignment.traceback.ref])
+    alignment = parasail.sw_trace_striped_32(ref, seq, 8, 4, parasail.dnafull)
+    align_tuple = (alignment.traceback.query, alignment.traceback.comp, alignment.traceback.ref)
 
-    return alignment, align_string
+    return alignment, align_tuple
