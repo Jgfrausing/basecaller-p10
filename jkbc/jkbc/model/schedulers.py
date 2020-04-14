34ca36381029b940b226424180a6142a7c4d3a36
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2020-04-14 15:34:18 +0200

    Fix import errors

--- jkbc/jkbc/model/schedulers.py
+++ jkbc/jkbc/model/schedulers.py
@@ -1,11 +1,14 @@
-import fastai.callbacks as fcb
-import fastai
 from functools import partial
+
+import fastai
+import fastai.callbacks as fcbs
+import fastai.callback as fcb
+
 import jkbc.types as t
 
 
 def get_named_schedulers(epochs, max_lr, moms: t.List[float],
-                         cycles: t.List[int]) -> t.Dict[str, t.Callable[[fastai.Learner], None]]:
+                         cycles: t.List[int]) -> t.Dict[str, t.Callable[[fastai.basic_train.Learner], None]]:
 
     named_schedulers = {"one_cycle": partial(
         attach_one_cycle_scheduler, epochs=epochs, min_lr=0, max_lr=max_lr)}
@@ -23,20 +26,20 @@ def get_named_schedulers(epochs, max_lr, moms: t.List[float],
 
 # Schedulers
 
-def attach_annealing_cos_scheduler(learner: fastai.Learner, epochs: int,
+def attach_annealing_cos_scheduler(learner: fastai.basic_train.Learner, epochs: int,
                                    min_lr: float, max_lr: float) -> None:
 
     remove_all_schedulers_from_learner(learner)
 
     total_iterations = calc_total_iterations(learner, epochs)
-    phase = fcb.TrainingPhase(total_iterations).schedule_hp(
+    phase = fcbs.TrainingPhase(total_iterations).schedule_hp(
         'lr', (max_lr, min_lr), fcb.annealing_cos)
-    scheduler = fcb.GeneralScheduler(learner, [phase])
+    scheduler = fcbs.GeneralScheduler(learner, [phase])
 
     add_scheduler_to_learner(learner, scheduler)
 
 
-def attach_annealing_cos_warmrestart_scheduler(learner: fastai.Learner, epochs: int, min_lr: float,
+def attach_annealing_cos_warmrestart_scheduler(learner: fastai.basic_train.Learner, epochs: int, min_lr: float,
                                                max_lr: float, mom: float, n_cycles) -> None:
 
     remove_all_schedulers_from_learner(learner)
@@ -44,15 +47,15 @@ def attach_annealing_cos_warmrestart_scheduler(learner: fastai.Learner, epochs: 
     total_iterations = calc_total_iterations(learner, epochs)
     splits = [2**n for n in range(n_cycles)]
     splits_norm = [s / sum(splits) for s in splits]
-    phases = [(fcb.TrainingPhase(total_iterations * splits_norm[i])
+    phases = [(fcbs.TrainingPhase(total_iterations * splits_norm[i])
                .schedule_hp('lr', (max_lr, min_lr), anneal=fcb.annealing_cos)
                .schedule_hp('mom', mom)) for i in range(n_cycles)]
-    scheduler = fcb.GeneralScheduler(learner, phases)
+    scheduler = fcbs.GeneralScheduler(learner, phases)
 
     add_scheduler_to_learner(learner, scheduler)
 
 
-def attach_one_cycle_scheduler(learner: fastai.Learner, epochs: int, min_lr: float,
+def attach_one_cycle_scheduler(learner: fastai.basic_train.Learner, epochs: int, min_lr: float,
                                max_lr: float) -> None:
 
     remove_all_schedulers_from_learner(learner)
@@ -62,27 +65,27 @@ def attach_one_cycle_scheduler(learner: fastai.Learner, epochs: int, min_lr: flo
     iterations_up = total_iterations * split
     iterations_down = total_iterations * (1 - split)
 
-    up_phase = fcb.TrainingPhase(iterations_up).schedule_hp(
+    up_phase = fcbs.TrainingPhase(iterations_up).schedule_hp(
         'lr', (min_lr, max_lr), anneal=fcb.annealing_cos)
-    down_phase = fcb.TrainingPhase(iterations_down).schedule_hp(
+    down_phase = fcbs.TrainingPhase(iterations_down).schedule_hp(
         'lr', (max_lr, min_lr), anneal=fcb.annealing_cos)
-    scheduler = fcb.GeneralScheduler(learner, [up_phase, down_phase])
+    scheduler = fcbs.GeneralScheduler(learner, [up_phase, down_phase])
 
     add_scheduler_to_learner(learner, scheduler)
 
 
 # Helpers
 
-def calc_total_iterations(learner: fastai.Learner, epochs: int) -> int:
+def calc_total_iterations(learner: fastai.basic_train.Learner, epochs: int) -> int:
     return len(learner.data.train_dl) * epochs
 
 
-def add_scheduler_to_learner(learner, scheduler: fcb.fcb.GeneralScheduler,
+def add_scheduler_to_learner(learner, scheduler: fcbs.GeneralScheduler,
                              remove_other_schedulers: bool = True) -> None:
     if remove_other_schedulers:
         remove_all_schedulers_from_learner(learner)
     schedulers_in_learner = [cb for cb in learner.callbacks if isinstance(
-        cb, (fcb.GeneralScheduler, fcb.Scheduler))]
+        cb, (fcbs.GeneralScheduler, fcbs.Scheduler))]
     if schedulers_in_learner:
         raise Exception("Multiple Schedulers",
                         f"Learner already contains the following schedulers: \
@@ -92,4 +95,4 @@ def add_scheduler_to_learner(learner, scheduler: fcb.fcb.GeneralScheduler,
 
 def remove_all_schedulers_from_learner(learner):
     learner.callbacks = list(filter(lambda cb: not isinstance(
-        cb, (fcb.GeneralScheduler, fcb.Scheduler)), learner.callbacks))
+        cb, (fcbs.GeneralScheduler, fcbs.Scheduler)), learner.callbacks))
