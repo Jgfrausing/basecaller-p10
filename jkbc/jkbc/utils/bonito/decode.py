b638215cdef08c922dd9174673ba08875c7784fc
jgfrausing <jfraus14@student.aau.dk>
2020-04-28 15:25:34 +0200

    global alignment instead

--- jkbc/jkbc/utils/bonito/decode.py
+++ jkbc/jkbc/utils/bonito/decode.py
@@ -29,7 +29,7 @@ def align(ref, seq):
     """
     Get (and print) the alignment between `ref` and `seq`
     """
-    alignment = parasail.sw_trace_striped_32(ref, seq, 8, 4, parasail.dnafull)
+    alignment = parasail.nw_trace_striped_32(ref, seq, 8, 4, parasail.dnafull)
     align_tuple = (alignment.traceback.query, alignment.traceback.comp, alignment.traceback.ref)
 
     return alignment, align_tuple
